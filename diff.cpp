4aca5a0f5e11a7e48aba02617ae3a3bc40409483
yj2285 <yj2285@columbia.edu>
2021-03-29 21:00:03 -0400

    virtual class

--- diff.cpp
+++ diff.cpp
@@ -1,23 +1,87 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <filesystem>
 
 /* create class to store the diff object (holding the result for each line)
  * consider character level vs line level diff
  * consier different output format
  */
-class Differ {
 
+class Differ {
 public:
-    Differ(string o, string u) :origin{o}, update{u} {}
-    void read() const {};
-    void compare() {};
-    void output() {};
+    virtual void read() {} = 0;
+    virtual void compare() {} = 0;
+    virtual void output() {} = 0;
+    virtual ~Differ() {}
+}
+
+
+class File_Differ : public Differ {
+public:
+    File_Differ(string o, string u);
+    void read() override;
+    void compare() override;
+    void output() override;
     ~Differ() {};
 
 private:
-    string origin, update;
+    string originf, updatef;
     vector<string> origin;
     vector<string> update
+}
 
+File_Differ::File_Differ(string o, string u) {
+    path f1 = o;
+    path f2 = u;
+    if (!exists(f1))
+        throw filesystem_error{"File does not exist: ", o};
+    if (!exists(f2))
+        throw filesystem_error{"File does not exist: ", u};
+    originf = o;
+    updatef = u;
+}
+
+
+void File_Differ::read {
+    string line;
+    ifstream origin_ifs {originf};
+    ifstream new_ifs {updatef};
+    if (origin_ifs.is_open())
+    {
+        while (getline(origin_ifs,line))
+        {
+            if (line.size() > 0) {
+                origin.push_back(line);
+            }
+        }
+        origin_ifs.close();
+    }
+
+    if (new_ifs.is_open())
+    {
+        while (getline(new_ifs,line))
+        {
+            if (line.size() > 0) {
+                update.push_back(line);
+            }
+        }
+        new_ifs.close();
+    }
+}
+
+void File_Differ::compare {
+
+}
+
+void File_Differ::output {
+
+}
+
+
+int main{
+    Differ differ {"", ""};
+    differ.read();
+    differ.compare();
+    differ.output();
 }
