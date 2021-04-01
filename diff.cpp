d0d4c4489ea542bc9771274fee49f32c7ad93812
yj2285 <yj2285@columbia.edu>
2021-04-01 00:56:25 -0400

    generate trace

--- diff.cpp
+++ diff.cpp
@@ -2,19 +2,16 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 //#include <filesystem>
 
-/* create class to store the diff object (holding the result for each line)
- * consider character level vs line level diff
- * consier different output format
- */
 using namespace std;
 
 class Differ {
 public:
     virtual void read() = 0;
-    virtual void compare() = 0;
-    virtual void output() = 0;
+    virtual int compare() = 0;
+    virtual void output(int) = 0;
     virtual ~Differ() {}
 };
 
@@ -23,8 +20,8 @@ class File_Differ : public Differ {
 public:
     File_Differ(const string&, const string&);
     void read() override;
-    void compare() override;
-    void output() override;
+    int compare() override;
+    void output(int) override;
     ~File_Differ() {};
 
 private:
@@ -32,10 +29,16 @@ private:
     const string& updatef;
     vector<string> origin;
     vector<string> update;
+    vector<vector<int>> trace;
 };
 
 File_Differ::File_Differ(const string& o, const string& u) 
     : originf {o}, updatef {u} {
+
+    /*
+     *path is not available unitl MacOS 10.15
+     */
+
     /*filesystem::path f1 = o;
     filesystem::path f2 = u;
     if (!exists(f1))
@@ -47,9 +50,6 @@ File_Differ::File_Differ(const string& o, const string& u)
 
 void File_Differ::read() {
     string line;
-
-    //cout << "originf is: " << originf << "\n";
-    //cout << "updatef is: " << updatef << "\n";
 
     ifstream origin_ifs {originf};
     ifstream new_ifs {updatef};
@@ -76,16 +76,55 @@ void File_Differ::read() {
     }
 }
 
-void File_Differ::compare() {
-    cout << "compare is called.\n";
+int File_Differ::compare() {
+    
+    int n = origin.size();
+    int m = update.size();
+    int max = n + m;
+    cout << "max is: " << max << "\n";
+
+    vector<int> v(2 * max + 1);
+    v[1+max] = 0;
+    int d, k, x, y;
+
+    for (d = 0; d <= max; d=d+1) {
+        
+        for (k = -d; k <= d; k=k+2) {
+            //cout << "k: " << k << "\n";
+            if ( k == -d || (k != d && v[k - 1 + max] < v[k + 1 + max])) {
+                x = v[k + 1 + max];
+            }
+            else {
+                x = v[k - 1 + max] + 1;
+            }
+            y = x - k;
+
+            while (x < n && y < m && origin[x] == update[y]) {
+                x = x + 1;
+                y = y + 1;
+            }
+            //cout << "x: " << x << "\n";
+            v[k + max] = x;
+            if (x >= n && y >= m) {
+                cout << "d: " << d << "\n";
+                trace.push_back(v);
+                return d;
+            }
+        }
+        trace.push_back(v);
+        //cout << "--------\n";
+    }
+    return max;
 }
 
-void File_Differ::output() {
-    for (auto& l : origin) {
-        cout << l << "\n";
-    }
-    for (auto& l : update) {
-        cout << l << "\n";
+void File_Differ::output(int real_d) {
+    cout << "size of trace: " << trace.size() << "\n";
+    reverse(trace.begin(), trace.end());
+    for (auto& t : trace) {
+        for (auto& v : t) {
+            cout << v << ",";
+        }
+        cout <<"\n";
     }
 }
 
@@ -96,11 +135,11 @@ int main(int argc, const char* argv[]) {
         cout << "Two files are expected\n.";
         return -1;
     }
-    
+
     string file1 {argv[1]};
     string file2 {argv[2]};
     File_Differ differ {file1, file2};
     differ.read();
-    differ.compare();
-    differ.output();
+    int d = differ.compare();
+    differ.output(d);
 }
